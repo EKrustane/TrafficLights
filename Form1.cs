1e859359138ef7ad937bb71ea21c4b7ea11c0a5b
Ermine <ermux12@gmail.com>
2020-06-03 19:20:06 +0300

    version 19:20

--- Form1.cs
+++ Form1.cs
@@ -51,30 +51,17 @@ namespace TrafficLights
 
         private void LightsCircle()
         {
-            int d = 100;
-            Rectangle red = new Rectangle(0, 0, RedLight.Width, RedLight.Height);
-            System.Drawing.Drawing2D.GraphicsPath gp1 = new System.Drawing.Drawing2D.GraphicsPath();
-            gp1.AddArc(red.X, red.Y, d, d, 180, 90);
-            gp1.AddArc(red.X + red.Width - d, red.Y, d, d, 270, 90);
-            gp1.AddArc(red.X + red.Width - d, red.Y + red.Height - d, d, d, 0, 90);
-            gp1.AddArc(red.X, red.Y + red.Height - d, d, d, 90, 90);
-            RedLight.Region = new Region(gp1);
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            path.AddEllipse(0, 0, RedLight.Width, RedLight.Height);
+            RedLight.Region = new Region(path);
 
-            Rectangle yellow = new Rectangle(0, 0, YellowLight.Width, RedLight.Height);
-            System.Drawing.Drawing2D.GraphicsPath gp2 = new System.Drawing.Drawing2D.GraphicsPath();
-            gp2.AddArc(yellow.X, yellow.Y, d, d, 180, 90);
-            gp2.AddArc(yellow.X + yellow.Width - d, yellow.Y, d, d, 270, 90);
-            gp2.AddArc(yellow.X + yellow.Width - d, yellow.Y + yellow.Height - d, d, d, 0, 90);
-            gp2.AddArc(yellow.X, yellow.Y + yellow.Height - d, d, d, 90, 90);
-            YellowLight.Region = new Region(gp2);
+            System.Drawing.Drawing2D.GraphicsPath path2 = new System.Drawing.Drawing2D.GraphicsPath();
+            path2.AddEllipse(0, 0, YellowLight.Width, YellowLight.Height);
+            YellowLight.Region = new Region(path2);
 
-            Rectangle green = new Rectangle(0, 0, GreenLight.Width, GreenLight.Height);
-            System.Drawing.Drawing2D.GraphicsPath gp3 = new System.Drawing.Drawing2D.GraphicsPath();
-            gp3.AddArc(green.X, green.Y, d, d, 180, 90);
-            gp3.AddArc(green.X + green.Width - d, green.Y, d, d, 270, 90);
-            gp3.AddArc(green.X + green.Width - d, green.Y + green.Height - d, d, d, 0, 90);
-            gp3.AddArc(green.X, green.Y + green.Height - d, d, d, 90, 90);
-            GreenLight.Region = new Region(gp3);
+            System.Drawing.Drawing2D.GraphicsPath path3 = new System.Drawing.Drawing2D.GraphicsPath();
+            path3.AddEllipse(0, 0, GreenLight.Width, GreenLight.Height);
+            GreenLight.Region = new Region(path3);
         }
     }
 }
